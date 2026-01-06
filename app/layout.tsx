09cc488afc9ddf3b878f61a30330415f4631159e
人間223 <149035267+radhi223@users.noreply.github.com>
2026-01-06 09:48:20 +0700

    cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,23 +10,18 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "SIAKKu - Manajemen Presensi",
   description: "Aplikasi Manajemen Presensi Mahasiswa untuk Dosen",
-  generator: "v0.app",
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/icon.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/icon.png",
         media: "(prefers-color-scheme: dark)",
       },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
     ],
-    apple: "/apple-icon.png",
+    apple: "/icon.png",
   },
 }
 
